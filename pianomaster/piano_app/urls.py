30235d5b7596efe38b86048bbcbc39fb0bba3949
Amandine Pinget PhD <78495096+AmandinePingetPhD@users.noreply.github.com>
2023-06-14 18:10:51 +0200

    Modify paths - in progress

--- pianomaster/piano_app/urls.py
+++ pianomaster/piano_app/urls.py
@@ -9,35 +9,22 @@ from django.contrib.auth.decorators import login_required
 
 app_name = 'piano_app'
 
-urlpatterns = [
-    path('', login_required(TemplateView.as_view(template_name='login.html'))),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
+# urlpatterns = [
+#     path('', login_required(TemplateView.as_view(template_name='login.html'))),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #path('identified', LogoutView.as_view(template_name='identified.html')),
 
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     path('suggestion', views.suggestion, name='suggestion'),
-# ]    
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('contact', views.contact, name='contact'),
+    # path('thanksContact', views.thankscontact, name='thanksContact'),
+    path('suggestion', views.suggestion, name='suggestion'),
+    path('suggestions', views.suggestions, name='suggestions'),
+    path('continueOrNot', views.continueornot, name='continueOrNot'),
+    path('continueOrNot/<int:rating>/', views.continueornot,
+         name='continueOrNot'),
+    path('toBeContinued', views.tobecontinued, name='toBeContinued'),
+    # path('404', views.custom_404, name='404'),
+    # path('500', views.custom_500, name='500'),
+]
 
-
-
-#     path('suggestions', views.suggestions, name='suggestions'),
-#     path('continueOrNot', views.continueornot, name='continueOrNot'),
-#     path('continueOrNot/<int:rating>/', views.continueornot,
-#          name='continueOrNot'),
-#     path('toBeContinued', views.tobecontinued, name='toBeContinued'),
-
-# ]
-#     path('contact', views.contact, name='contact'),
-#     path('thanksContact', views.thankscontact, name='thanksContact'),
-#     path('suggestion', views.suggestion, name='suggestion'),
-#     path('suggestions', views.suggestions, name='suggestions'),
-#     path('continueOrNot', views.continueornot, name='continueOrNot'),
-#     path('continueOrNot/<int:rating>/', views.continueornot,
-#          name='continueOrNot'),
-#     path('toBeContinued', views.tobecontinued, name='toBeContinued'),
-#     path('404', views.custom_404, name='404'),
-#     path('500', views.custom_500, name='500'),
-# ]
